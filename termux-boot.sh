f59cef17106b740ae90b55b2348c7df3f62db032
Gapkobob <36904758+Gapkobob@users.noreply.github.com>
2024-03-27 14:53:19 +0300

    termux-boot.sh

--- termux-boot.sh
+++ termux-boot.sh
@@ -1,1 +1,4 @@
-laucnh com.termux
+am startservice --user 0 -n com.termux/com.termux.app.RunCommandService \
+-a com.termux.RUN_COMMAND \
+--es com.termux.RUN_COMMAND_PATH '/data/data/com.termux/files/usr/bin/login' \
+--ez com.termux.RUN_COMMAND_BACKGROUND 'false' 
